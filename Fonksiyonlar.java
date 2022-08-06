1541f67004cfe6d171a063367a6ef2aae544f985
Farukyl <99757704+Farukyl@users.noreply.github.com>
2022-08-06 10:41:04 +0300

    Add files via upload

---
+++ Fonksiyonlar.java
@@ -0,0 +1,53 @@
+
+public class Fonksiyonlar {
+	
+	public static int toplama(int a, int b) {
+		return(a+b) ;
+		
+	}
+	
+	
+	
+	public static int carpma(int a, int b) {
+		return(a*b);
+		
+		
+	}
+	
+	
+	
+	public static int bölme(int a, int b) {
+		return(a/b);
+		
+		
+		
+	}
+	
+	
+	public static int cýkartma(int a, int b) {
+		return(a-b);
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Sonuç=" +toplama(15,20));
+		
+		System.out.println("Sonuç=" +carpma(10,20));
+		
+		System.out.println("Sonuç=" +bölme(50,5));
+		
+		System.out.println("Sonuç=" +cýkartma(557,329));
+		
+
+		
+		
+
+	}
+
+}
